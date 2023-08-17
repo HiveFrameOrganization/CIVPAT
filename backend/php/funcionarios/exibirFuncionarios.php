1387a3a68c795abb1da10634cd06b1880ee83912
Nathan Barros <nathansousa0809@gmail.com>
2023-08-17 17:26:53 -0300

    Começando com a lista de funcionarios

---
+++ backend/php/funcionarios/exibirFuncionarios.php
@@ -0,0 +1,54 @@
+<?php
+header('Access-Control-Allow-Origin: http://localhost:8080');
+header('Access-Control-Allow-Methods: GET');
+header('Access-Control-Allow-Headers: Content-Type');
+header("Content-Type: application/json");
+
+// Buscando o arquivo do banco:
+require_once '../../../database/conn.php';
+
+function retornaFuncionarios($conn)
+{
+    // preparando a query
+    $stmt = $conn->prepare("SELECT NIF, Nome, Sobrenome, Email, TipoUser FROM Usuarios");
+
+    // Excutando a query
+    $stmt->execute();
+
+    $resultado = $stmt->get_result();
+
+    $usuarios = array();
+
+    // Iterando sobre os resultados e armazenando em um array
+    while ($row = $resultado->fetch_assoc()) {
+        $usuarios[] = $row;
+    }
+
+    // Enviando a resposta do servidor
+    $resposta = [
+        'status' => 'sucesso',
+        'mensagem' => 'Usuários retornados com sucesso',
+        'usuarios' => $usuarios
+    ];
+
+
+    echo json_encode($resposta);
+
+}
+
+// Veirifcando o tipo da requisição
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+    retornaFuncionarios($conn);
+
+} else {
+
+    // Enviando uma resposta de erro para o frontend
+    $resposta = [
+        "mensagem" => 'Algo deu errado...',
+        'status' => 'erro'
+    ];
+
+    echo json_encode($resposta);
+}
+?>
