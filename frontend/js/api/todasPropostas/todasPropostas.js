dd1edde7509d2a4df97390fd25c1b2e464177413
michaelsalmeida <michaellsantosalmeida@gmail.com>
2023-08-18 16:25:49 -0300

    tela home iniciada

--- frontend/js/api/todasPropostas/todasPropostas.js
+++ frontend/js/api/todasPropostas/todasPropostas.js
@@ -1,8 +1,8 @@
 window.addEventListener('load', () => {
-    pegasTodasAsPropostas()
+    pegarTodasAsPropostas()
 })
 
-async function pegasTodasAsPropostas () {
+async function pegarTodasAsPropostas () {
 
     try{
         const resposta = await fetch('http://localhost:8080/backend/php/todasPropostas/todasPropostas.php');
@@ -13,8 +13,20 @@ async function pegasTodasAsPropostas () {
 
         console.log(dados)
 
+        // console.log(dados.length)  
+        
+        exibirPropostas(dados.propostas);
+
+
     } catch (error){
         console.error(error)
     }
 
+}
+
+
+exibirPropostas(propostas) {
+    for (porposta of propostas) {
+        
+    }
 }
