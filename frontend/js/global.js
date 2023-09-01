dc26f488071ababf9f92af0bac2f7a64b9bed163
marcellu-s <marcellusfran@outlook.com>
2023-09-01 14:48:40 -0300

    UPDATED: Melhor experiência do usuario (Sidebar)

--- frontend/js/global.js
+++ frontend/js/global.js
@@ -5,6 +5,13 @@ const profileDropdown = document.querySelector('#profile-dropdown');
 
 window.addEventListener('click', (event) => {
 
+    if (!event.target.matches('#sidebar') && !event.target.matches('.sidebar-btn')) {
+
+        sidebar.classList.contains('left-0') && sidebar.classList.replace('left-0', '-left-full');
+
+        fade.classList.add('hide');
+    }
+
     if (event.target.matches('#profile-trigger')) {
 
         profileDropdown.classList.toggle('hidden');
@@ -13,6 +20,7 @@ window.addEventListener('click', (event) => {
     }
 
     profileDropdown.classList.add('hidden');
+
 });
 
 profileDropdown.addEventListener('click', (event) => {
@@ -26,13 +34,21 @@ profileDropdown.addEventListener('click', (event) => {
 
 const sidebar = document.querySelector('#sidebar');
 const sidebarBtn = document.querySelectorAll('.sidebar-btn');
+const fade = document.querySelector('#fade');
 
 sidebarBtn.forEach((btn) => {
 
     btn.addEventListener('click', () => {
 
         sidebar.classList.contains('-left-full') ? sidebar.classList.replace('-left-full', 'left-0') : sidebar.classList.replace('left-0', '-left-full');
+
+        fade.classList.toggle('hide');
     });
+});
+
+sidebar.addEventListener('click', (event) => {
+
+    event.stopPropagation();
 });
 
 // ------------------------------------------------------------------
