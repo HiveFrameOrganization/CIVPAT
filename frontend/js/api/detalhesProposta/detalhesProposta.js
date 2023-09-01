f2d8151911f0c31d85ebcc8f1cc04ecb3e00ae33
michaelsalmeida <michaellsantosalmeida@gmail.com>
2023-09-01 15:31:14 -0300

    dropdown dos gerentes implementados

--- frontend/js/api/detalhesProposta/detalhesProposta.js
+++ frontend/js/api/detalhesProposta/detalhesProposta.js
@@ -7,6 +7,7 @@ window.addEventListener('load', () => {
     verificarBancoProposta(idProposta);
     verificarPdfExistente(idProposta);
     carregarProdutos(idProposta);
+    carregarTecnicos();
 
 })
 
@@ -102,7 +103,7 @@ async function verificarBancoProposta(id){
         const dataUltimoProduto = document.querySelector('#dataUltimoProduto').value = resposta['dataUltimoProduto'];
         const valorTotalProdutos = document.querySelector('#valorTotalProdutos').value = resposta['valorTotalProdutos'];
         const primeiroGerente = document.querySelector('#primeiroGerente').value = resposta['Gerentes'][0]['Nome']; 
-        const segundoGerente = document.querySelector('#segundoGerente').value = resposta['Gerentes'][1]['Nome'];
+        // const segundoGerente = document.querySelector('#segundoGerente').value = resposta['Gerentes'][1]['Nome'];
         const funil = document.querySelector('#funil').value = resposta['funil'];
         const momeContato = document.querySelector('#momeContato').value = resposta['momeContato'];
         const emailContato = document.querySelector('#emailContato').value = resposta['emailContato'];
@@ -193,6 +194,55 @@ function baixarPdf (tipoPdf) {
 }
 
 
+// function validarCNPJ(cnpj) {
+//     const cnpjRecebido = cnpj;
+
+//     const cnpjString = cnpjRecebido.toString();
+
+//     const cnpjSemDigitosFinais = cnpjString.slice(0, -2);
+
+//     console.log(cnpjSemDigitosFinais);
+
+
+// }
+
+function validarCNPJ(cnpj) {
+    // Remover caracteres não numéricos
+    cnpj = cnpj.replace(/\D/g, '');
+  
+    // Verificar se o CNPJ tem 14 dígitos
+    if (cnpj.length !== 14) {
+      return false;
+    }
+  
+    // Verificar se todos os dígitos são iguais (números repetidos não são válidos)
+    if (/^(\d)\1+$/.test(cnpj)) {
+      return false;
+    }
+  
+    // Calcular o primeiro dígito verificador
+    let soma = 0;
+    for (let i = 0; i < 12; i++) {
+      soma += parseInt(cnpj.charAt(i)) * (13 - i);
+    }
+    soma = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+    if (parseInt(cnpj.charAt(12)) !== soma) {
+      return false;
+    }
+  
+    // Calcular o segundo dígito verificador
+    soma = 0;
+    for (let i = 0; i < 13; i++) {
+      soma += parseInt(cnpj.charAt(i)) * (14 - i);
+    }
+    soma = (soma % 11) < 2 ? 0 : 11 - (soma % 11);
+    if (parseInt(cnpj.charAt(13)) !== soma) {
+      return false;
+    }
+  
+    return true;
+  }
+
 
 
 
@@ -216,61 +266,30 @@ editandoProposta.addEventListener('click', () =>{
     }
 
 
-    const idProposta = localStorage.getItem('idProposta');
-    //Pegando os valores dos input's para transformalos em objeto
-    const nomeProposta = document.querySelector('#tituloProposta').value;
-    const cnpj = document.querySelector('#cnpj').value;
-    const uniCriadora= document.querySelector('#uniCriadora').value;
-    const empresa = document.querySelector('#empresa').value;
-    const statusProposta = document.querySelector('#statusProposta').value;
-    const criadorProposta = document.querySelector('#criadorProposta').value;
-    const numeroSGSET = document.querySelector('#numeroSGSET').value;
-    const primeiroGerente = document.querySelector('#primeiroGerente').value;
-    const segundoGerente = document.querySelector('#segundoGerente').value ;
-    const funil = document.querySelector('#funil').value;
-    const momeContato = document.querySelector('#momeContato').value;
-    const emailContato = document.querySelector('#emailContato').value;
-    const numeroContato = document.querySelector('#numeroContato').value; 
-   
-    // validarCNPJ(cnpj)
     
-    // // Função para fazer o cálculo do CNPJ
-    // function validarCNPJ(cnpj) {
-
-    //     // Verificar se o CNPJ possui 14 dígitos após a remoção dos não numéricos
-    //     if (cnpj.length !== 14) {
-    //     return false;
-    //     }
-    
-    //     // Calcular o primeiro dígito verificador
-    //     for(let digito = 0; digito < 2; digito++) {
-
-    //         let sum = 0; let num = 0;
-
-    //         for (let i = 5 + digito; i > 1; i--) {
-    //         sum += parseInt(cnpj[num]) * i;
-    //         num++;
-    //         }
-
-    //         for (let i = 9; i > 1; i--) {
-    //             sum += parseInt(cnpj[num]) * i;
-    //             num++;
-    //         }
-
-    //         // Definição dos digitos validos
-    //         if (digito == 0) {
-
-    //             digito1 = sum % 11 < 2 ? 0 : 11 - (sum % 11);
-    //         } else {
-
-    //             digito2 = sum % 11 < 2 ? 0 : 11 - (sum % 11);
-    //         }
-    //     }
+    if(editandoProposta.value == 'Editar'){
+        const idProposta = localStorage.getItem('idProposta');
+        //Pegando os valores dos input's para transformalos em objeto
+        const nomeProposta = document.querySelector('#tituloProposta').value;
+        const cnpj = document.querySelector('#cnpj').value;
+        const uniCriadora= document.querySelector('#uniCriadora').value;
+        const empresa = document.querySelector('#empresa').value;
+        const statusProposta = document.querySelector('#statusProposta').value;
+        const criadorProposta = document.querySelector('#criadorProposta').value;
+        const numeroSGSET = document.querySelector('#numeroSGSET').value;
+        const primeiroGerente = document.querySelector('#primeiroGerente').value;
+        const segundoGerente = document.querySelector('#segundoGerente').value ;
+        const funil = document.querySelector('#funil').value;
+        const momeContato = document.querySelector('#momeContato').value;
+        const emailContato = document.querySelector('#emailContato').value;
+        const numeroContato = document.querySelector('#numeroContato').value; 
         
-    //     if (parseInt(cnpj[12]) !== digito1 || parseInt(cnpj[13]) !== digito2) {
-    //         console.log("Falhou")
+        const verificacaoDoCnpj = validarCNPJ(cnpj);
         
-    //     } else {
+            
+        if (verificacaoDoCnpj == false) {
+            alert('CNPJ inválido');
+        } else {
         // Criando um objeto com os dados dos input's
             const detalhesProposta = {
                 idProposta: idProposta,
@@ -291,9 +310,8 @@ editandoProposta.addEventListener('click', () =>{
             
             // Enviando o objeto para o back end
             postarDetalhesBanco(detalhesProposta);
-    //     }
-  
-    // }
+        }
+    }
     
 });
 
@@ -371,5 +389,39 @@ async function exibirProdutos(produtos) {
         // Adicionando uma quebra de linha entre os botões
         const quebraDeLinha = document.createElement('br');
         botoes.appendChild(quebraDeLinha);
+    }
+}
+
+
+
+
+async function carregarTecnicos () {
+
+    const gerente1Dropdown = document.getElementById('primeiroGerente');
+    const gerente2Dropdown = document.getElementById('segundoGerente');
+
+
+    const requisicao = await fetch(back + '/cadastroProduto/carregarTecnicos.php', {
+        methods : 'GET'
+    });
+
+    const resposta = await requisicao.json();
+
+    for (var i = 0; i < resposta.length; i++) {
+        var optionElement = document.createElement("option");
+        optionElement.value = resposta[i + 1];
+        optionElement.textContent = resposta[i];
+        gerente1Dropdown.appendChild(optionElement);
+
+        i += 1;
+    }
+
+    for (var i = 0; i < resposta.length; i++) {
+        var optionElement = document.createElement("option");
+        optionElement.value = resposta[i + 1];
+        optionElement.textContent = resposta[i];
+        gerente2Dropdown.appendChild(optionElement);
+
+        i += 1;
     }
 }
