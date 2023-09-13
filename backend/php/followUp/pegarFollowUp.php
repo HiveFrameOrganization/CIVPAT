a8f1de8662fdf65320a5bf30dd86193e034ddcfa
ViolettVohor <scarlettaphroditeoho@gmail.com>
2023-09-12 21:34:50 -0300

    Update do Historico

--- backend/php/followUp/pegarFollowUp.php
+++ backend/php/followUp/pegarFollowUp.php
@@ -9,7 +9,7 @@ require_once '../../../database/conn.php';
 if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Pega os status de funil da tabela StatusFunil
     $stmt = $conn->prepare("SELECT `idFollowUp`,`Usuarios`.Nome, `Usuarios`.Sobrenome, `Data`, `Comentario`, `DataProxFollowUp`
-    FROM FollowUp 
+    FROM FollowUp
     INNER JOIN Usuarios ON `FollowUp`.`fk_nifUsuario` = `Usuarios`.`NIF`
     WHERE fk_idProposta = ? ORDER BY `Data`");
     $stmt->bind_param("s", $_GET['idProposta']);
--- frontend/js/api/historicoFunil/historicoFunil.js
+++ frontend/js/api/historicoFunil/historicoFunil.js
@@ -1,8 +1,9 @@
 import { back } from '../Rotas/rotas.js'
 const div = document.getElementById('tabelaHist');
 
+const idProposta = localStorage.getItem('idProposta');
 // Acessa o backend para pegar os historicos do funil de vendas
-fetch(back + `/followUp/pegarHistoricoFunil.php?idProposta=1`, {method: 'GET'})
+fetch(back + `/historico/pegarHistoricoFunil.php?idProposta=${idProposta}`, {method: 'GET'})
 .then(response => {
     if (!response.ok) {
         throw new Error(`Request failed with status: ${response.status} ${response.statusText}`);
@@ -11,15 +12,30 @@ fetch(back + `/followUp/pegarHistoricoFunil.php?idProposta=1`, {method: 'GET'})
 })
 .then(data => {
     // Loop para criar os ps
-    if (data.mensagem) {
-        
-    } else {
-        data.forEach((status) => {
-            const p = document.createElement('p');
-            p.textContent = status; // Adiciona o texto do p (Para poder ser visto na tela)
-            div.appendChild(p); // Adiciona o p à div
-        });
-    }
+    data.forEach((historico) => {
+        const statusAtualP = document.createElement('p');
+        const statusAnteriorP = document.createElement('p');
+        const dataInicialP = document.createElement('p');
+        const dataFinalP = document.createElement('p');
+        const dataDiferencaP = document.createElement('p');
+
+        let dataInicial = new Date(historico[2]);
+        let dataFinal = new Date(historico[3]);
+        // Calcule a diferença em dias
+        let diferencaEmDias = (dataFinal - dataInicial) / (1000 * 60 * 60 * 24);
+
+        statusAtualP.textContent = "Status Atual: " + historico[0];
+        statusAnteriorP.textContent = "Status Anterior: " + historico[1];
+        dataInicialP.textContent = "Data Inicial: " + historico[2];
+        dataFinalP.textContent = "Data Final: " + historico[3]; // Adiciona o texto do p (Para poder ser visto na tela)
+        dataDiferencaP.textContent = "Diferença de Datas: " + diferencaEmDias + " dias";
+
+        div.appendChild(statusAtualP); // Adiciona o p à div
+        div.appendChild(statusAnteriorP);
+        div.appendChild(dataInicialP);
+        div.appendChild(dataFinalP);
+        div.appendChild(dataDiferencaP);
+    });
 })
 .catch(error => {
     console.error('Error:', error);
