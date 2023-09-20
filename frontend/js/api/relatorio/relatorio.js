cb079026527b30d439267074f9b6beff1c379ebe
mayckl2 <mayckluciano2@gmail.com>
2023-09-20 17:26:54 -0300

    estilizaçao tela de relatorio

--- frontend/js/api/relatorio/relatorio.js
+++ frontend/js/api/relatorio/relatorio.js
@@ -12,11 +12,10 @@ import { back } from "../Rotas/rotas.js";
     indefinido. Importante para a lógica do back-end
 */
 
-// teste
 window.addEventListener('load', ()=>{
-    buscarRelatorio(9, 2023, 1234560)
+    exibirRelatorio(null)
 })
-// fim teste
+
 
 async function buscarRelatorio(mes, ano, valor = false) {
 
@@ -106,29 +105,65 @@ formularioRelatorio.addEventListener('submit', async evento => {
 // FUNÇÃO DE EXIBIR RELATORIO NA TELA
 async function exibirRelatorio(res){
     let exibir = document.querySelector('#exibir')
+    
+    exibir.innerHTML = ''
 
-    console.log(res.dados[0].NomeProduto)
+    // console.log(res.dados[0].NomeProduto)
 
-    for (let i = 0; i < res.dados.length; i++) {
-        // console.log(i);
-        
-        exibir.innerHTML += `
-            <div>
-                <p class='text-color-text'>${res.dados[i].Nome} ${res.dados[i].Sobrenome}</p>
-                <p class='text-color-text'>${res.dados[i].NIF}</p>
-                <p class='text-color-text'>${res.dados[i].TituloProposta}</p>
-                <p class='text-color-text'>${res.dados[i].NomeProduto}</p>
-                <p class='text-color-text'>${res.dados[i].Datas}</p>
-                <p class='text-color-text'>${res.dados[i].HorasPessoa}</p>
+    if(res == null){
 
-                ${
-                    res.dados[i].Maquina != 'Nenhum' ? 
-                        `<p class='text-color-text'>${res.dados[i].Maquina}</p>
-                        <p class='text-color-text'>${res.dados[i].HorasMaquina}</p>`:
-                        `<p class='text-color-text'>Nenhuma maquina sendo usada</p>`   
-                }
-            </div>
-            <hr>
+        exibir.innerHTML = `
+            <h1 class='text-color-text font-semibold text-lg leading-4 uppercase text-center'>
+                Insira mês e ano da pesquisa e NIF do funcionario desejado!
+            </h1>
         `
-    }
-}
+
+    }else{
+
+        for (let i = 0; i < res.dados.length; i++) {
+            
+            exibir.innerHTML += `
+            <div class='bg-component pt-4 rounded-xl flex overflow-x-scroll w-full'>
+            
+            <div class='border-r-2 border-[gray] px-8 flex flex-col gap-2'>
+            <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>${res.dados[i].Nome} ${res.dados[i].Sobrenome}</p>
+            <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1'>${res.dados[i].NIF}</p>
+            </div>
+                    
+                    <div class='border-r-2 border-[gray] px-8 flex flex-col gap-2'>
+                    <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>${res.dados[i].TituloProposta}</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1'>Produto</p>
+                    </div>
+                    
+                    <div class='border-r-2 border-[gray] px-8 flex flex-col gap-2'>
+                    <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>${res.dados[i].NomeProduto}</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1'>Proposta</p>
+                    </div>
+                    
+                    
+                    <div class='border-r-2 border-[gray] px-8 flex flex-col gap-2'>
+                    <p class='text-color-text font-semibold text-lg leading-4 capitalize'>${res.dados[i].Datas}</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1 whitespace-nowrap'>Data de lançamento</p>
+                    </div>
+                    
+                    <div class='border-r-2 border-[gray] px-8 flex flex-col gap-2'>
+                    <p class='text-color-text font-semibold text-lg leading-4 capitalize'>${res.dados[i].HorasPessoa}</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1 whitespace-nowrap'>Horas lançadas</p>
+                    </div>
+
+                    ${
+                        res.dados[i].Maquina != 'Nenhum' ? 
+                        `
+                        <div class='px-8 flex flex-col gap-2'>
+                        <p class='text-color-text font-semibold text-lg leading-4 capitalize'>${res.dados[i].Maquina}</p>
+                        <p class='text-color-text font-semibold text-lg leading-4 capitalize'>${res.dados[i].HorasMaquina}</p>
+                        </div>`:
+                        `<div class='px-8 flex flex-col gap-2 justify-center'>
+                        <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>Nenhuma maquina sendo usada</p>
+                        </did>`   
+                    }
+                    </div>
+                    `
+                }
+            }
+        }
