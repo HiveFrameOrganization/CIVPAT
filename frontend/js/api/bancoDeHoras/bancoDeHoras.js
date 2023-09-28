222d6492479172d1a6d3d97467ee4e2d4b238b43
mayckl2 <mayckluciano2@gmail.com>
2023-09-28 14:31:06 -0300

    estilização bando de horas

--- frontend/js/api/bancoDeHoras/bancoDeHoras.js
+++ frontend/js/api/bancoDeHoras/bancoDeHoras.js
@@ -29,7 +29,7 @@ formularioBancoHoras.addEventListener('submit', async evento => {
     console.log(`Data correta`);
     await buscarHoras(mes, ano, nif, cargo);
 
-    
+
 
 });
 
@@ -39,7 +39,7 @@ async function buscarHoras(mes, ano, nif, cargo) {
     try {
 
         console.log(nif)
-        
+
         if (!nif) throw new Error(`É nescessário o NIF do funcionário para pesquisar`);
 
         const requisicao = await fetch(`${back}/relatorio/puxarRelatorio.php?mes=${mes}&ano=${ano}&valor=${nif}&cargo=${cargo}`);
@@ -47,6 +47,7 @@ async function buscarHoras(mes, ano, nif, cargo) {
         const resposta = await requisicao.json();
 
         console.log(resposta);
+        exibirRelatorio(resposta)
 
     } catch (error) {
         console.error(error);
@@ -80,4 +81,96 @@ function dataMaxima(mes, ano) {
     }
 
     return false;
+}
+
+
+// FUNÇÃO DE EXIBIR RELATORIO NA TELA
+async function exibirRelatorio(res) {
+
+    exibir.innerHTML = ''
+
+    // console.log(res.dados[0].NomeProduto)
+
+    // QUANDO CARREGAR A PAGINA E NÃO OUVER NENHUM RELATORIO GERADO
+    if (res == null) {
+
+        exibir.innerHTML = `
+            <h1 class='text-color-text font-semibold text-lg leading-4 uppercase text-center'>
+                Insira mês e ano da pesquisa e NIF do funcionario desejado!
+            </h1>
+        `
+
+    } else {
+
+        // CRIANDO ELEMENTO QUE SERAO INSERIDOS OS DADOS
+        let cabeçalho = document.createElement('div')
+        cabeçalho.classList = 'flex bg-body rounded-t-xl py-8'
+        let horas = document.createElement('div')
+        horas.classList = 'rounded-b-xl bg-component flex flex-col overflow-y-hidden pb-4 transition-height mb-8'
+
+        for (let i = 0; i < res.dados.length; i++) {
+
+            let nif
+            let proposta
+            let produto
+            let horaTotal = 0
+
+            // CALCULA TODAS AS HORAS FEITAS NO MÊS E SALVA NUMA VARIAVEL
+            for (let x = 0; x < res.dados.length; x++) {
+                horaTotal += parseInt(res.dados[x].HorasPessoa)
+            }
+
+            // VERIFICAR SE AS HORAS CADASTRADAS PERTENCEM AO MESMO TECNICO
+            if (nif == null) {
+                nif = res.dados[i].NIF
+
+                if (proposta == null && produto == null || proposta != res.dados[i].TituloProposta && res.dados[i].NomeProduto)
+                    proposta = res.dados[i].TituloProposta
+                produto = res.dados[i].NomeProduto
+
+                cabeçalho.innerHTML = `
+                    <div class='border-r-2 border-[gray] px-8 flex flex-col gap-2'>
+                    <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>${localStorage.getItem('nomeLogin')}</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1'>Técnico</p>
+                    </div>
+                    
+                    <div class='border-r-2 border-[gray] px-8 flex flex-col gap-2'>
+                    <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>${res.dados[i].NomeProduto}</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1'>Produto</p>
+                    </div>
+                    
+                    <div class='border-r-2 border-[gray] px-8 flex flex-col gap-2'>
+                    <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>${res.dados[i].TituloProposta}</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1'>Proposta</p>
+                    </div>
+
+                    <div class='border-[gray] px-8 flex flex-col gap-2'>
+                    <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>${horaTotal} Horas</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1'>Total de horas no mês</p>
+                    </div>
+                    `
+
+            }
+
+            horas.innerHTML += `
+            <div class='bg-body p-4 mx-4 mt-4 flex overflow-x-scroll w-[98,5%] rounded-xl min-h-[4rem]'>
+            
+                    <div class='border-r-2 border-[gray] px-8 flex flex-col gap-2 bg-body'>
+                    <p class='text-color-text font-semibold text-lg leading-4 capitalize'>${res.dados[i].Datas}</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1 whitespace-nowrap'>Data de lançamento</p>
+                    </div>
+                    
+                    <div class=' border-[gray] px-8 flex flex-col gap-2 bg-body'>
+                    <p class='text-color-text font-semibold text-lg leading-4 capitalize'>${res.dados[i].HorasPessoa}  Horas</p>
+                    <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1 whitespace-nowrap'>Horas lançadas</p>
+                    </div>
+
+                    
+                    </div>
+                    `
+
+            exibir.appendChild(cabeçalho)
+            exibir.appendChild(horas)
+        }
+    }
 }
