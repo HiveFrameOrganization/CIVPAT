b52f5b211b99303e7fc9a1895958a89560aed522
mayckl2 <mayckluciano2@gmail.com>
2023-10-05 16:59:43 -0300

    conserto produtos

--- frontend/js/api/detalhesProposta/exibirProdutos.js
+++ frontend/js/api/detalhesProposta/exibirProdutos.js
@@ -2,7 +2,7 @@ export default async function exibirProdutos(produtos) {
     // selecionando a div dos botões
     const botoes = document.getElementById('propostas');
     // limpando os possíveis elementos que possam estar na div
-    document.getElementsByClassName('tabela')[0].innerHTML = '';
+    document.getElementById('propostas').innerHTML = '';
     
     for (let produto of produtos) {
         console.log(produto['Situacao'])
