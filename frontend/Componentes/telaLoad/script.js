89a99eeb6484762d663235db4c1d3114fa2ad6e2
leosfl9 <leonardosilvaflores29@gmail.com>
2023-10-05 17:02:03 -0300

    limpando o console

--- frontend/Componentes/telaLoad/script.js
+++ frontend/Componentes/telaLoad/script.js
@@ -1,6 +1,5 @@
 // SOME TELA DE CARREGAMENTO APOS CARREGAMENTO DA PAGINA
 function someCarregamento(){
-    console.log('carregou')
     setTimeout(()=> document.querySelector('#carregamento').classList.add('hidden'),
         1000
     )
