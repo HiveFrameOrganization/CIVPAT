57adfa8c148d9f8ebb6b32fc672291446755dc96
mayckl2 <mayckluciano2@gmail.com>
2023-10-10 14:38:05 -0300

    desativando botao de declinar numa proposta ja declinada

--- frontend/js/api/detalhesProposta/desativaBotoes.js
+++ frontend/js/api/detalhesProposta/desativaBotoes.js
@@ -2,6 +2,7 @@ import { editandoProposta } from "./detalhesProposta.js"
 
 // DESATIVA BOTÃO DE EDITAR E NOVO PRODUTO QUANDO NAO ESTA MAIS EM ANALISE
 export default function desativaBotoes(){
+    let declinarProposta = document.querySelector('#declinarProposta')
     if(localStorage.getItem('statusProposta') != 'Em Análise'){
         editandoProposta.setAttribute('disabled', 'true')
         editandoProposta.classList.add('disabled:opacity-20')
@@ -14,5 +15,10 @@ export default function desativaBotoes(){
         btnNovoProduto.classList.remove('hover:text-primary')
         btnNovoProduto.classList.remove('hover:bg-[transparent]')
         btnNovoProduto.classList.add('disabled:opacity-20')
+
+        declinarProposta.setAttribute('disabled', 'true')
+        declinarProposta.classList.add('disabled:opacity-20')
+        declinarProposta.classList.remove('hover:bg-color-red/40')
+        declinarProposta.classList.remove('cursor-pointer')
     }
 }
