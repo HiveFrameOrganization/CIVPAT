8823515e9194945722fdf9facb77c270ed196495
leosfl9 <leonardosilvaflores29@gmail.com>
2023-10-19 17:02:50 -0300

    desabilitando todos os botoes do detalhe proposta caso esteja concluida ou declinada

--- frontend/js/api/detalhesProposta/desativaBotoes.js
+++ frontend/js/api/detalhesProposta/desativaBotoes.js
@@ -22,6 +22,8 @@ export default function desativaBotoes(){
         declinarProposta.classList.add('disabled:opacity-20')
         declinarProposta.classList.remove('hover:bg-color-red/40')
         declinarProposta.classList.remove('cursor-pointer')
+        declinarProposta.classList.remove('hover:bg-[transparent]')
+        declinarProposta.classList.remove('hover:text-color-red')
     }
 
     if(localStorage.getItem('statusProposta') == 'Aceito' || localStorage.getItem('statusProposta') == 'Declinado' || localStorage.getItem('statusProposta') == 'Concluido'){
@@ -30,6 +32,32 @@ export default function desativaBotoes(){
         botaoAceitar.classList.add('disabled:opacity-20');
         botaoAceitar.classList.remove('hover:bg-btn-blue/40');
         botaoAceitar.classList.remove('cursor-pointer');
+        botaoAceitar.classList.remove('hover:bg-[transparent]');
+        botaoAceitar.classList.remove('hover:text-color-green')
+
+        let botaoSalvar = document.querySelector('#editarProposta');
+        botaoSalvar.classList.remove('hover:text-primary');
+        botaoSalvar.classList.remove('hover:bg-[transparent]')
+    }
+
+    if(localStorage.getItem('statusProposta') == 'Declinado' || localStorage.getItem('statusProposta') == 'Concluido'){
+        let botaoPDF = document.querySelector('#botaoSalvarPdf');
+        botaoPDF.setAttribute('disabled', 'true');
+        botaoPDF.classList.add('disabled:opacity-20');
+        botaoPDF.classList.remove('hover:outline')
+        botaoPDF.classList.remove('hover:text-primary')
+        botaoPDF.classList.remove('hover:bg-btn-blue/40');
+        botaoPDF.classList.remove('hover:bg-[transparent]')
+        botaoPDF.classList.remove('cursor-pointer');
+
+        let botaoFollowUp = document.querySelector('#adicionar');
+        botaoFollowUp.setAttribute('disabled', 'true');
+        botaoFollowUp.classList.add('disabled:opacity-20');
+        botaoFollowUp.classList.remove('hover:outline')
+        botaoFollowUp.classList.remove('hover:text-primary')
+        botaoFollowUp.classList.remove('hover:bg-btn-blue/40');
+        botaoFollowUp.classList.remove('hover:bg-[transparent]')
+        botaoFollowUp.classList.remove('cursor-pointer');
     }
 
     
