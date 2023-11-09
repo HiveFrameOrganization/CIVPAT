17e8774fd6e5c8ae1b63cdb3b1777c8a573a4680
luizbrito6 <luizg1717@gmail.com>
2023-11-09 10:07:27 -0300

    fix: Conectando banco local

--- database/banco/4-users.sql
+++ database/banco/4-users.sql
@@ -1,7 +1,7 @@
 -- Cria o usuário auth que representa a autenticação
-CREATE USER 'auth'@'172.20.0.3' IDENTIFIED BY 'fsY9DS&J*bg#49Vpf^9o';
+CREATE USER 'auth'@'%' IDENTIFIED BY 'fsY9DS&J*bg#49Vpf^9o';
 -- Este usuário será utilizado no login, para conferir as credenciais
-GRANT SELECT, UPDATE ON `isihiveframe`.`Usuarios` TO 'auth'@'172.20.0.3';
+GRANT SELECT, UPDATE ON `isihiveframe`.`Usuarios` TO 'auth'@'%';
 FLUSH PRIVILEGES;
 
 
@@ -36,69 +36,69 @@ GRANT SELECT ON `isihiveframe`.`vw_gerentesResponsaveis` TO role1;
 
 
 -- Cria o usuário coor que representa os Coordenadores
-CREATE USER 'coor'@'172.20.0.3' IDENTIFIED BY 'g0J153!eRB64lEsY3SNa';
+CREATE USER 'coor'@'%' IDENTIFIED BY 'g0J153!eRB64lEsY3SNa';
 -- Concede o role1 ao usuário ger
-GRANT role1 TO 'coor'@'172.20.0.3';
+GRANT role1 TO 'coor'@'%';
 -- Concede ao coor a habilidade de inserir e alterar as tabelas abaixo
-GRANT INSERT, UPDATE ON `isihiveframe`.`StatusFunil` TO 'coor'@'172.20.0.3';
-GRANT INSERT, UPDATE ON `isihiveframe`.`TipoPDF` TO 'coor'@'172.20.0.3';
-GRANT INSERT, UPDATE ON `isihiveframe`.`ServicoCategoria` TO 'coor'@'172.20.0.3';
-GRANT INSERT, UPDATE ON `isihiveframe`.`NomeProduto` TO 'coor'@'172.20.0.3';
-GRANT INSERT, UPDATE ON `isihiveframe`.`Usuarios` TO 'coor'@'172.20.0.3';
-GRANT INSERT, UPDATE ON `isihiveframe`.`Maquinas` TO 'coor'@'172.20.0.3';
-GRANT INSERT ON `isihiveframe`.`CargaHoraria` TO 'coor'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`PDF` TO 'coor'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`vw_lancamentoHoras` TO 'coor'@'172.20.0.3';
+GRANT INSERT, UPDATE ON `isihiveframe`.`StatusFunil` TO 'coor'@'%';
+GRANT INSERT, UPDATE ON `isihiveframe`.`TipoPDF` TO 'coor'@'%';
+GRANT INSERT, UPDATE ON `isihiveframe`.`ServicoCategoria` TO 'coor'@'%';
+GRANT INSERT, UPDATE ON `isihiveframe`.`NomeProduto` TO 'coor'@'%';
+GRANT INSERT, UPDATE ON `isihiveframe`.`Usuarios` TO 'coor'@'%';
+GRANT INSERT, UPDATE ON `isihiveframe`.`Maquinas` TO 'coor'@'%';
+GRANT INSERT ON `isihiveframe`.`CargaHoraria` TO 'coor'@'%';
+GRANT SELECT ON `isihiveframe`.`PDF` TO 'coor'@'%';
+GRANT SELECT ON `isihiveframe`.`vw_lancamentoHoras` TO 'coor'@'%';
 FLUSH PRIVILEGES;
 
 
 -- Cria o usuário ger que representa os Gerentes
-CREATE USER 'ger'@'172.20.0.3' IDENTIFIED BY 'OqxWqi!erb9a&F9Ar750';
+CREATE USER 'ger'@'%' IDENTIFIED BY 'OqxWqi!erb9a&F9Ar750';
 -- Concede o role1 ao usuário ger
-GRANT role1 TO 'ger'@'172.20.0.3';
+GRANT role1 TO 'ger'@'%';
 -- Concede ao ger a habilidade de inserir e alterar as tabelas abaixo
-GRANT INSERT ON `isihiveframe`.`CargaHoraria` TO 'ger'@'172.20.0.3';
-GRANT INSERT, UPDATE ON `isihiveframe`.`Usuarios` TO 'ger'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`PDF` TO 'ger'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`vw_lancamentoHoras` TO 'ger'@'172.20.0.3';
+GRANT INSERT ON `isihiveframe`.`CargaHoraria` TO 'ger'@'%';
+GRANT INSERT, UPDATE ON `isihiveframe`.`Usuarios` TO 'ger'@'%';
+GRANT SELECT ON `isihiveframe`.`PDF` TO 'ger'@'%';
+GRANT SELECT ON `isihiveframe`.`vw_lancamentoHoras` TO 'ger'@'%';
 FLUSH PRIVILEGES;
 
 
 -- Cria o usuário adm que representa a Administração
-CREATE USER 'adm'@'172.20.0.3' IDENTIFIED BY 'GPbhjBMD2T9%k&91FND5';
+CREATE USER 'adm'@'%' IDENTIFIED BY 'GPbhjBMD2T9%k&91FND5';
 -- Concede o role1 ao usuário adm 
-GRANT role1 TO 'adm'@'172.20.0.3';
+GRANT role1 TO 'adm'@'%';
 
-GRANT INSERT, UPDATE ON `isihiveframe`.`Usuarios` TO 'adm'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`PDF` TO 'adm'@'172.20.0.3';
+GRANT INSERT, UPDATE ON `isihiveframe`.`Usuarios` TO 'adm'@'%';
+GRANT SELECT ON `isihiveframe`.`PDF` TO 'adm'@'%';
 FLUSH PRIVILEGES;
 
 
 -- Cria o usuário tec que representa os Tecnicos
-CREATE USER 'tec'@'172.20.0.3' IDENTIFIED BY '3HY1O9%g5YrcAVu4cVR#';
+CREATE USER 'tec'@'%' IDENTIFIED BY '3HY1O9%g5YrcAVu4cVR#';
 -- Este terá acesso a visualização das tabelas abaixo
-GRANT SELECT ON `isihiveframe`.`Propostas` TO 'tec'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`ServicoCategoria` TO 'tec'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`NomeProduto` TO 'tec'@'172.20.0.3';
-GRANT SELECT, UPDATE ON `isihiveframe`.`Produtos` TO 'tec'@'172.20.0.3';
-GRANT SELECT, UPDATE ON `isihiveframe`.`Usuarios` TO 'tec'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`Maquinas` TO 'tec'@'172.20.0.3';
+GRANT SELECT ON `isihiveframe`.`Propostas` TO 'tec'@'%';
+GRANT SELECT ON `isihiveframe`.`ServicoCategoria` TO 'tec'@'%';
+GRANT SELECT ON `isihiveframe`.`NomeProduto` TO 'tec'@'%';
+GRANT SELECT, UPDATE ON `isihiveframe`.`Produtos` TO 'tec'@'%';
+GRANT SELECT, UPDATE ON `isihiveframe`.`Usuarios` TO 'tec'@'%';
+GRANT SELECT ON `isihiveframe`.`Maquinas` TO 'tec'@'%';
 -- e só poderá inserir ou alterar na tabela Carga Horária
-GRANT SELECT, INSERT, UPDATE ON `isihiveframe`.`CargaHoraria` TO 'tec'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`UnidadeCriadora` TO 'tec'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`vw_produtos` TO 'tec'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`vw_lancamentoHoras` TO 'tec'@'172.20.0.3';
-GRANT SELECT ON `isihiveframe`.`vw_relatorioSemMaquina` TO 'tec'@'172.20.0.3';
+GRANT SELECT, INSERT, UPDATE ON `isihiveframe`.`CargaHoraria` TO 'tec'@'%';
+GRANT SELECT ON `isihiveframe`.`UnidadeCriadora` TO 'tec'@'%';
+GRANT SELECT ON `isihiveframe`.`vw_produtos` TO 'tec'@'%';
+GRANT SELECT ON `isihiveframe`.`vw_lancamentoHoras` TO 'tec'@'%';
+GRANT SELECT ON `isihiveframe`.`vw_relatorioSemMaquina` TO 'tec'@'%';
 
 FLUSH PRIVILEGES;
 
 
 -- Ativa os Roles de cada Usuário
 SET DEFAULT ROLE ALL TO
-  'coor'@'172.20.0.3',
-  'adm'@'172.20.0.3',
-  'ger'@'172.20.0.3',
-  'tec'@'172.20.0.3';
+  'coor'@'%',
+  'adm'@'%',
+  'ger'@'%',
+  'tec'@'%';
 
 
 -- Criar um usuário para ser utilizado durante os testes
