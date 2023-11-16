4f50afceb42977229ff1ee76b36779f496802615
Ribamar <ribamarnfalmada@gmail.com>
2023-11-16 15:31:11 -0300

    :bug: fix: Total de horas menstal aparece em toodos os produtos.

--- frontend/js/api/relatorio/relatorio.js
+++ frontend/js/api/relatorio/relatorio.js
@@ -226,7 +226,7 @@ async function exibirRelatorio(res) {
                     </div>
 
                     <div class='border-[gray] px-8 flex flex-col gap-2'>
-                    <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>${horaTotal} Horas</p>
+                    <p class='text-color-text whitespace-nowrap font-semibold text-lg leading-4 capitalize'>${Number(res.dados[i].HorasPessoa) + Number(res.dados[i].HorasMaquina)} Horas</p>
                     <p class='text-color-text-secundary font-semibold text-xs flex flex-wrap justify-between gap-1'>Total de horas no mês</p>
                     </div>
 
