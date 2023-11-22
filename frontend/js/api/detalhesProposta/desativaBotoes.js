0b5d6f51fc2cfb56cf3753393623cc5aa6acacc5
marcellu-s <marcellus.costa@outlook.com>
2023-11-22 17:16:55 -0300

    :bug: fix: Refatoração em desativar botões + Estilização

--- frontend/js/api/detalhesProposta/desativaBotoes.js
+++ frontend/js/api/detalhesProposta/desativaBotoes.js
@@ -2,80 +2,52 @@ import { editandoProposta } from "./detalhesProposta.js"
 
 // DESATIVA BOTÃO DE EDITAR E NOVO PRODUTO QUANDO NAO ESTA MAIS EM ANALISE
 export default function desativaBotoes(){
-    let declinarProposta = document.querySelector('#declinarProposta')
-    // if(localStorage.getItem('statusProposta') == 'Em Análise'){
-    //     console.log('em analise ou solicitada')
-    // }else{
-    //     editandoProposta.setAttribute('disabled', 'true')
-    //     editandoProposta.classList.add('disabled:opacity-20')
-    //     editandoProposta.classList.remove('hover:bg-btn-blue/40')
-    //     editandoProposta.classList.remove('cursor-pointer')
-    
-    //     let btnNovoProduto = document.querySelector('#btnNovoProduto')
-    //     btnNovoProduto.setAttribute('disabled', 'true')
-    //     btnNovoProduto.classList.remove('hover:outline')
-    //     btnNovoProduto.classList.remove('hover:text-primary')
-    //     btnNovoProduto.classList.remove('hover:bg-[transparent]')
-    //     btnNovoProduto.classList.add('disabled:opacity-20')
 
-    //     declinarProposta.setAttribute('disabled', 'true')
-    //     declinarProposta.classList.add('disabled:opacity-20')
-    //     declinarProposta.classList.remove('hover:bg-color-red/40')
-    //     declinarProposta.classList.remove('cursor-pointer')
-    //     declinarProposta.classList.remove('hover:bg-[transparent]')
-    //     declinarProposta.classList.remove('hover:text-color-red')
-    // }
+    // Botões 
+    const btnDeclinar = document.querySelector('#declinarProposta');
+    const btnAceitar = document.querySelector('#aceitarProposta');
+    const btnEditar = document.querySelector('#editarProposta');
 
-    let botaoAceitar = document.querySelector('#aceitarProposta');
-    if(localStorage.getItem('statusProposta') == 'Aceito' || localStorage.getItem('statusProposta') == 'Declinado' || localStorage.getItem('statusProposta') == 'Concluido'){
-        
-        botaoAceitar.parentElement.removeChild(botaoAceitar)
+    if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'em análise') {
+
+        //PASS
+    } else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'aceito') {
+
+        btnAceitar.parentElement.removeChild(btnAceitar);
+        btnDeclinar.parentElement.removeChild(btnDeclinar);
+    } else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'concluido') {
+
+        btnAceitar.parentElement.removeChild(btnAceitar);
+        btnDeclinar.parentElement.removeChild(btnDeclinar);
+        btnEditar.parentElement.removeChild(btnEditar);
+    } else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'declinado') {
+
+        btnAceitar.parentElement.removeChild(btnAceitar);
+        btnDeclinar.parentElement.removeChild(btnDeclinar);
+        btnEditar.parentElement.removeChild(btnEditar);
+    }  else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'solicitação de aceite') {
+
+        if (localStorage.getItem('cargo').toLocaleLowerCase() != 'adm') {
+
+            btnAceitar.parentElement.removeChild(btnAceitar);
+            btnDeclinar.parentElement.removeChild(btnDeclinar);
+            btnEditar.parentElement.removeChild(btnEditar);
+        } else {
+
+            btnDeclinar.parentElement.removeChild(btnDeclinar);
+            btnEditar.parentElement.removeChild(btnEditar);
+        }
+    } else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'solicitação de declinio') {
+
+        if (localStorage.getItem('cargo').toLocaleLowerCase() != 'adm') {
+
+            btnAceitar.parentElement.removeChild(btnAceitar);
+            btnDeclinar.parentElement.removeChild(btnDeclinar);
+            btnEditar.parentElement.removeChild(btnEditar);
+        } else {
+
+            btnEditar.parentElement.removeChild(btnEditar);
+            btnAceitar.parentElement.removeChild(btnAceitar);
+        }
     }
-
-    if(localStorage.getItem('statusProposta') == 'Declinado' || localStorage.getItem('statusProposta') == 'Concluido'){
-        let botaoPDF = document.querySelector('#botaoSalvarPdf');
-
-        botaoPDF.parentElement.removeChild(botaoPDF)
-
-        let botaoFollowUp = document.querySelector('#adicionar');
-        
-        botaoFollowUp.parentElement.removeChild(botaoFollowUp)
-    }
-
-    if (localStorage.getItem('statusProposta') == 'Solicitação de Declinio'){
-
-        botaoAceitar.parentElement.removeChild(botaoAceitar)
-
-        // botaoAceitar.setAttribute('disabled', 'true');
-        // botaoAceitar.classList.add('disabled:opacity-20');
-        // botaoAceitar.classList.remove('hover:bg-btn-blue/40');
-        // botaoAceitar.classList.remove('cursor-pointer');
-        // botaoAceitar.classList.remove('hover:bg-[transparent]');
-        // botaoAceitar.classList.remove('hover:text-color-green')
-
-        editandoProposta.setAttribute('disabled', 'true')
-        editandoProposta.classList.add('disabled:opacity-20')
-        editandoProposta.classList.remove('hover:bg-btn-blue/40')
-        editandoProposta.classList.remove('cursor-pointer')
-    } 
-
-    if (localStorage.getItem('statusProposta') == 'Solicitação de Aceite'){
-        let botaoAceitar = document.querySelector('#aceitarProposta');
-        botaoAceitar.removeAttribute('disabled');
-        botaoAceitar.classList.add('disabled:opacity-20');
-        botaoAceitar.classList.add('hover:bg-btn-blue/40');
-        botaoAceitar.classList.add('cursor-pointer');
-        botaoAceitar.classList.add('hover:bg-[transparent]');
-        botaoAceitar.classList.add('hover:text-color-green')
-
-        botaoDeclinar.removeAttribute('disabled');
-        editandoProposta.removeAttribute('disabled')
-        editandoProposta.classList.add('hover:bg-btn-blue/40')
-        editandoProposta.classList.add('cursor-pointer')
-        botaoDeclinar.classList.add('cursor-pointer')
-        botaoDeclinar.classList.add('hover:bg-[transparent]')
-        botaoDeclinar.classList.add('hover:text-color-red')
-
-    }
-    
 }
