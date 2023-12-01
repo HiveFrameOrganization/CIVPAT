2db77b3be19abd4f0e6b78b930d0b9ec7b04f279
leosfl9 <leonardosilvaflores29@gmail.com>
2023-12-01 15:12:02 -0300

    refactor: renderizando botoes de acordo com status da proposta

--- frontend/js/api/detalhesProposta/desativaBotoes.js
+++ frontend/js/api/detalhesProposta/desativaBotoes.js
@@ -5,44 +5,103 @@ export default function desativaBotoes(){
     const btnDeclinar = document.querySelector('#declinarProposta');
     const btnAceitar = document.querySelector('#aceitarProposta');
     const btnEditar = document.querySelector('#editarProposta');
+    const btnConcluir = document.querySelector('#concluirProposta');
+    const btnPdf = document.querySelector('#botaoSalvarPdf');
+    const inputsPdf = document.querySelectorAll('.pdf');
+    const btnFollowUp = document.querySelector('#adicionar');
+    const editFollowUp = document.querySelectorAll('.fa-edit');
+    const btnProduto = document.querySelector('#btnNovoProduto');
 
-    if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'aceito') {
+    // REMOVENDO BOTÕES DE PROPOSTA EM ANÁLISE
+    if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'em análise'){
+        btnConcluir.parentElement.removeChild(btnConcluir);
+
+
+    // REMOVENDO BOTÕES DE PROPOSTA ACEITAS
+    } else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'aceito') {
 
         btnAceitar.parentElement.removeChild(btnAceitar);
         btnDeclinar.parentElement.removeChild(btnDeclinar);
+        btnEditar.parentElement.removeChild(btnEditar);
+        btnProduto.parentElement.removeChild(btnProduto);
+
+    // REMOVENDO BOTÕES DE PROPOSTA CONCLUÍDAS
     } else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'concluido') {
 
         btnAceitar.parentElement.removeChild(btnAceitar);
         btnDeclinar.parentElement.removeChild(btnDeclinar);
         btnEditar.parentElement.removeChild(btnEditar);
+        btnConcluir.parentElement.removeChild(btnConcluir);
+        btnPdf.parentElement.removeChild(btnPdf);
+        btnFollowUp.parentElement.removeChild(btnFollowUp);
+        inputsPdf.forEach((input) => {
+            input.disabled = true;
+        });
+        editFollowUp.forEach((icon) => {
+            icon.parentElement.removeChild(icon);
+        })
+        btnProduto.parentElement.removeChild(btnProduto);
+    
+    // REMOVENDO BOTÕES DE PROPOSTA DECLINADAS
     } else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'declinado') {
 
         btnAceitar.parentElement.removeChild(btnAceitar);
         btnDeclinar.parentElement.removeChild(btnDeclinar);
         btnEditar.parentElement.removeChild(btnEditar);
+        btnConcluir.parentElement.removeChild(btnConcluir);
+        btnPdf.parentElement.removeChild(btnPdf);
+        btnFollowUp.parentElement.removeChild(btnFollowUp);
+        inputsPdf.forEach((input) => {
+            input.disabled = true;
+        });
+        editFollowUp.forEach((icon) => {
+            icon.parentElement.removeChild(icon);
+        })
+        btnProduto.parentElement.removeChild(btnProduto);
+
     }  else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'solicitação de aceite') {
 
-        if (localStorage.getItem('cargo').toLocaleLowerCase() != 'adm') {
+        if (localStorage.getItem('cargo').toLocaleLowerCase() == 'ger' || localStorage.getItem('cargo').toLocaleLowerCase() == 'tec') {
 
             btnAceitar.parentElement.removeChild(btnAceitar);
             btnDeclinar.parentElement.removeChild(btnDeclinar);
             btnEditar.parentElement.removeChild(btnEditar);
-        } else {
+            btnConcluir.parentElement.removeChild(btnConcluir);
+            btnPdf.parentElement.removeChild(btnPdf);
+            btnFollowUp.parentElement.removeChild(btnFollowUp);
+            inputsPdf.forEach((input) => {
+                input.disabled = true;
+            });
+            editFollowUp.forEach((icon) => {
+                icon.parentElement.removeChild(icon);
+            })
+            btnProduto.parentElement.removeChild(btnProduto);
 
-            btnDeclinar.parentElement.removeChild(btnDeclinar);
-            btnEditar.parentElement.removeChild(btnEditar);
+        } else {
+            btnConcluir.parentElement.removeChild(btnConcluir);
         }
+
     } else if (localStorage.getItem('statusProposta').toLocaleLowerCase() == 'solicitação de declinio') {
 
-        if (localStorage.getItem('cargo').toLocaleLowerCase() != 'adm') {
+        // REMOVENDO BOTÕES DE PROPOSTAS EM SOLICITAÇÃO DE DECLÍNIO
+        if (localStorage.getItem('cargo').toLocaleLowerCase() == 'ger' || localStorage.getItem('cargo').toLocaleLowerCase() == 'tec') {
 
             btnAceitar.parentElement.removeChild(btnAceitar);
             btnDeclinar.parentElement.removeChild(btnDeclinar);
             btnEditar.parentElement.removeChild(btnEditar);
-        } else {
+            btnConcluir.parentElement.removeChild(btnConcluir);
+            btnPdf.parentElement.removeChild(btnPdf);
+            btnFollowUp.parentElement.removeChild(btnFollowUp);
+            inputsPdf.forEach((input) => {
+                input.disabled = true;
+            });
+            editFollowUp.forEach((icon) => {
+                icon.parentElement.removeChild(icon);
+            })
+            btnProduto.parentElement.removeChild(btnProduto);
 
-            btnEditar.parentElement.removeChild(btnEditar);
-            btnAceitar.parentElement.removeChild(btnAceitar);
+        } else {
+            btnConcluir.parentElement.removeChild(btnConcluir);
         }
     }
 }
